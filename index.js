784578321ebd02eaa456f1d6ae7b2860ac512b49
Tushar Jain <tusharjain2201@gmail.com>
2023-01-13 18:54:06 +0530

    Add server which reads json data of coordinates

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
-app.use(express.static("public"));
+app.use(express.static("public")); // host files in public folder
+app.use(express.json()); // parse json response
+app.post("/api", (request, response) => {
+    console.log(request.body);
+    const data = request.body;
+    response.json({
+        latitude: data.latitude,
+        longitude: data.longitude
+    });
+})
