dd2185a650750c53942d6938c6b0dcb2e804764d
Tushar Jain <tusharjain2201@gmail.com>
2023-01-18 00:07:36 +0530

    Add database

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const Datastore = require("nedb");
+
 const app = express();
+const db = new Datastore("database.db");
+
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public")); // host files in public folder
 app.use(express.json()); // parse json response
+
+db.loadDatabase();
+
 app.post("/api", (request, response) => {
     console.log(request.body);
     const data = request.body;
+    data.timestamp = Date.now();
+    db.insert(data)
     response.json({
         latitude: data.latitude,
-        longitude: data.longitude
+        longitude: data.longitude,
+        timestamp: data.timestamp
     });
 })
