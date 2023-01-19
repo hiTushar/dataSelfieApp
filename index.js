33cc7031bc4d246e5c540eb1ffcaf0f736bdf34e
hiTushar <70627686+hiTushar@users.noreply.github.com>
2023-01-19 11:09:59 +0000

    add image capture feature

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const db = new Datastore("database.db");
 
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public")); // host files in public folder
-app.use(express.json()); // parse json response
+app.use(express.json({ limit: "1mb" })); // parse json response
 
 db.loadDatabase();
 
 app.post("/api", (request, response) => {
-    console.log(request.body);
-    const data = request.body;
-    data.timestamp = Date.now();
-    db.insert(data)
-    response.json(data);
+  console.log(request.body);
+  const data = request.body;
+  data.timestamp = Date.now();
+  db.insert(data)
+  response.json(data);
 })
 
 app.get("/all", (request, response) => {
-    db.find({}, (err, docs) => {
-        if(err) {
-            response.end();
-        }
-        else {
-            response.json(docs);
-        }
-    })
+  db.find({}, (err, docs) => {
+    if (err) {
+      response.end();
+    }
+    else {
+      response.json(docs);
+    }
+  })
 })
